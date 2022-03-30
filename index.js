5338a6dc73e550a95f1d524063222e345b9f2496
olenasmokvyna <olenasmokvyna@bymason.com>
2022-03-30 12:11:49 -0700

    Hello world

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+//run "node index.js" in the current directory and load http://localhost:3000/ in a browser to see the output
---
+++ practice/oblects.ts
@@ -0,0 +1,9 @@
+var animals = {
+  pets: ["dogs", "cats"],
+  wild: ["tiger"],
+  rescued: true,
+  adopted: {
+    pets: 5,
+    wild: 2,
+  },
+};
